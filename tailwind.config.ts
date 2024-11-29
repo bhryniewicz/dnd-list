94e3206b7b1cc24894b11d011a6c25f471c657fe
Bartosz Hryniewicz <bartosz.hryniewicz3@gmail.com>
2024-11-29 14:26:18 +0100

    feat(NVP-28): push all changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,11 +5,35 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/screens/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       boxShadow: {
-        "3xl": "0 1px 2px 0 rgba(16, 24, 40, 0.05)",
+        link: "0 1px 2px 0 rgba(16, 24, 40, 0.05)",
+      },
+      border: {
+        DEFAULT: "1px",
+      },
+      colors: {
+        border: {
+          primary: "#D0D5DD",
+          secondary: "#D6BBFB",
+          noLinks: "#EAECF0",
+        },
+        background: {
+          primary: "#F9FAFB",
+          secondary: "#f5f5f5",
+          accent: "#7F56D9",
+        },
+        font: {
+          primary: "#344054",
+          secondary: "#6941C6",
+          title: "#101828",
+          subtitle: "#475467",
+          input: "#667085",
+          error: "#de3a5b",
+        },
       },
     },
   },
